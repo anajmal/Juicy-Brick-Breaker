8064b68a812bb8e290e1eecbdee6cd682a39f5e6
anajmal <adilnajmal123@gmail.com>
2023-12-15 10:29:38 -0500

    Update

--- Global.gd
+++ Global.gd
@@ -6,24 +6,37 @@ var score = 0
 var lives = 0
 var time = 0
 var fever = 0
-var fever_multiplier = 0.15
 var starting_in = 0
+
+var color_rotate = 0
+var color_rotate_amount = 10
+var color_rotate_index = 0.01
+var color_position = Vector2.ZERO
+
+var sway_index = 0
+var sway_period = 0.1
 
 var fever_decay = 0.1
 var feverish = false
 
 
-var default_starting_in = 4
-var default_lives = 5
+@export var default_starting_in = 4
+@export var default_lives = 5
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	randomize()
 	VP = get_viewport().size
-	get_tree().get_root().size_changed.connect(_resize)
+	var _signal = get_tree().get_root().connect("size_changed", Callable(self, "_resize"))
 	reset()
 
 func _physics_process(_delta):
+	if color_rotate >= 0:
+		color_rotate -= color_rotate_index
+		color_rotate_index *= 1.05
+	else:
+		color_rotate_index = 0.1
+	sway_index += sway_period
 	if fever >= 100 and not feverish:
 		fever = 100
 	elif fever > 0:
@@ -43,7 +56,7 @@ func _input(event):
 				get_tree().paused = false
 				Pause_Menu.hide()
 			else:
-				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 				get_tree().paused = true
 				Pause_Menu.show()
 	if fever >= 100 and event.is_action_pressed("fever"):
@@ -76,7 +89,7 @@ func update_lives(l):
 		end_game(false)
 
 func update_fever(f):
-	fever += f * fever_multiplier
+	fever += f
 	var HUD = get_node_or_null("/root/Game/UI/HUD")
 	if HUD != null:
 		HUD.update_fever()
@@ -87,15 +100,15 @@ func update_time(t):
 	if HUD != null:
 		HUD.update_time()
 	if time <= 0:
-		next_level()
+		end_game(false)
 
 func next_level():
 	level += 1
 	fever = 0
-	get_tree().change_scene_to_file("res://Game.tscn")
+	var _scene = get_tree().change_scene_to_file("res://Game.tscn")
 
 func end_game(success):
 	if success:
-		get_tree().change_scene_to_file("res://UI/End_Game.tscn")
+		var _scene = get_tree().change_scene_to_file("res://UI/End_Game.tscn")
 	else:
-		get_tree().change_scene_to_file("res://UI/End_Game.tscn")
+		var _scene = get_tree().change_scene_to_file("res://UI/End_Game.tscn")
--- UI/End_Game.gd
+++ UI/End_Game.gd
@@ -1,4 +1,14 @@
 extends Control
 
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	$Label.text = "Thanks for playing! Your final score was " + str(Global.score) + "."
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func _on_play_pressed():
+	Global.reset()
+	get_tree().change_scene_to_file("res://Game.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
